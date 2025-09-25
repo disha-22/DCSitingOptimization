049e0ae221b8062e4326055f3e920f5f12db9e27
richardachen <richyperson@gmail.com>
2025-09-25 09:23:13 -0700

    Implementing validation

--- Model_PSCC_Draft_RC.py
+++ Model_PSCC_Draft_RC.py
@@ -7,6 +7,7 @@ import pandas as pd
 import geopandas as gpd
 import pickle
 import configparser
+import copy
 
 # tracking
 import wandb
@@ -15,7 +16,7 @@ import wandb
 import os
 
 def prepare_optimization_data(huc8_df, solar_proportion_df, wind_proportion_df,
-                            demand_profile, data_center_cost=6e5, train_path=None):
+                            demand_profile, data_center_cost=6e5):
     """
     Prepare all data matrices for the optimization problem
 
@@ -31,8 +32,6 @@ def prepare_optimization_data(huc8_df, solar_proportion_df, wind_proportion_df,
             Numpy array with time series of total data center demand profile
         data_center_cost: float
             Cost of data center, in ($/MWh-year)
-        train_path: string
-            Whether to include data from a trained optimization file. If it's a string, then it should point to a path of `train' results.
 
     Returns
     -------
@@ -55,10 +54,6 @@ def prepare_optimization_data(huc8_df, solar_proportion_df, wind_proportion_df,
             D: demand profile
             Y: existing data center capacity in each location
             huc8_order: order of HUC8 for each data array
-            (if train_path was inputted: we can return the optimized results for renewables validation.)
-            x: optimized data center capacity
-            s: optimized solar capacity
-            w: optimized wind capacity
     """
 
     L = len(huc8_df)
@@ -101,38 +96,15 @@ def prepare_optimization_data(huc8_df, solar_proportion_df, wind_proportion_df,
     # Existing capacity (assume zero for now, can be updated)
     Y = np.zeros((L, T)) # we reshape to (L, T) in order to ensure easier broadcasting within the optimization model.
 
-    # Validation results
-    if train_path is not None:
-        # read in values for x, s, w
-        with open(train_path, "rb") as f:
-            train_results = pickle.load(f)
-
-        x = train_results['results']['x']
-        s = train_results['results']['s']
-        w = train_results['results']['w']
-
-        return {
-            'L': L, 'T': T,
-            'P_dc': P_dc, 'P_g': P_g, 'P_s': P_s, 'P_w': P_w,
-            'S_dc': S_dc, 'S_g': S_g, 'S_s': S_s, 'S_w': S_w,
-            'E_g': E_g, 'E_s': E_s, 'E_w': E_w,
-            'C_s': C_s, 'C_w': C_w,
-            'D': D, 'Y': Y,
-            'huc8_order': huc8_order,
-            'x': x,
-            's': s,
-            'w': w
-        }
-    else:
-        return {
-            'L': L, 'T': T,
-            'P_dc': P_dc, 'P_g': P_g, 'P_s': P_s, 'P_w': P_w,
-            'S_dc': S_dc, 'S_g': S_g, 'S_s': S_s, 'S_w': S_w,
-            'E_g': E_g, 'E_s': E_s, 'E_w': E_w,
-            'C_s': C_s, 'C_w': C_w,
-            'D': D, 'Y': Y,
-            'huc8_order': huc8_order
-        }
+    return {
+        'L': L, 'T': T,
+        'P_dc': P_dc, 'P_g': P_g, 'P_s': P_s, 'P_w': P_w,
+        'S_dc': S_dc, 'S_g': S_g, 'S_s': S_s, 'S_w': S_w,
+        'E_g': E_g, 'E_s': E_s, 'E_w': E_w,
+        'C_s': C_s, 'C_w': C_w,
+        'D': D, 'Y': Y,
+        'huc8_order': huc8_order
+    }
 
 
 def compute_composite_costs(data, alpha, beta, gamma, normalization='all_std'):
@@ -203,19 +175,19 @@ def compute_composite_costs(data, alpha, beta, gamma, normalization='all_std'):
     return M_g, M_s, M_w, norm_S, norm_P, norm_E
 
 
-def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='max', verbose=True, grid_only=False, normalization='all_std', train_path=None):
+def optimize_data_center_siting(config, scenario_name, weights_dict, equity_type='max', verbose=True, grid_only=False, normalization='all_std', train_path=None):
     """
     Solve the data center siting optimization problem
 
     Parameters:
     -----------
-    data : dict
-        Dictionary containing all prepared data
+    config: dict
+        Dictionary containing all configuration specifications
     scenario_name: string
         Name of scenario being computed
     weights_dict: dict
         Dictionary of weights, where keys are weights names and values are dictionaries of (alpha, beta, gamma, delta) weights
-    equity_type : str
+    equity_type: str
         'max' for maximum water scarcity or 'mad' for mean absolute difference
     verbose: boolean
         True if we want to print updates as optimization is in progress.
@@ -231,6 +203,18 @@ def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='
         Path to results from a trained optimization model. Applies when we are running a validation case.    
     """
 
+    # TODO Richard fix
+    # read data
+    huc8_df = gpd.read_file(config['huc8_df'])
+    solar_proportion_df = pd.read_csv(config['solar_proportion_df'], index_col=0)
+    wind_proportion_df = pd.read_csv(config['wind_proportion_df'], index_col=0)
+    demand_profile = pd.read_csv(config['demand_profile'], index_col=0)
+
+    data = prepare_optimization_data(
+            huc8_df, solar_proportion_df, wind_proportion_df, demand_profile
+        )
+
+
     # ==================== Parameters =====================
     # set alpha, beta, gamma, delta as parameters (speeds up solving if we do multiple runs with different parameters: https://www.cvxpy.org/tutorial/intro/index.html#parameters)
     alpha = cp.Parameter(nonneg=True)
@@ -238,7 +222,12 @@ def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='
     gamma = cp.Parameter(nonneg=True)
     delta = cp.Parameter(nonneg=True)
 
-
+    if train_path is not None:
+        train_x = cp.Parameter(nonneg=True)
+        train_s = cp.Parameter(nonneg=True)
+        train_w = cp.Parameter(nonneg=True)
+        
+        
     # ==================== Data =======================
     # L (number of locations) and T (number of time steps) constants
     L, T = data['L'], data['T']
@@ -283,14 +272,6 @@ def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='
     # ======================= Constraints ======================
     constraints = []
 
-    # Enforce fixed variables for validation optimization models
-    train_constraints = []
-    if train_path is not None:
-        train_constraints.append(x == data['x'])
-        train_constraints.append(s == data['s'])
-        train_constraints.append(w == data['w'])
-    constraints.extend(train_constraints)
-
     # Meet demand (equation 7)
     constraints.append(cp.sum(a, axis=0) >= data['D'])
 
@@ -311,6 +292,14 @@ def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='
             constraints.append(s[l] == 0)
             constraints.append(w[l] == 0)
 
+    # Enforce fixed variables for validation optimization models
+    if train_path is not None:
+        train_constraints = []
+        train_constraints.append(x == train_x)
+        train_constraints.append(s == train_s)
+        train_constraints.append(w == train_w)
+        constraints.extend(train_constraints)
+
 
     # ==================== Objective function (equation 6) ====================
     obj = ((beta / norm_P) * (data['P_dc'].T @ x) +
@@ -320,9 +309,9 @@ def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='
            (alpha / norm_S) * (data['S_dc'].T @ cp.sum(a, axis=1)) +
            (delta / norm_S) * f_equity)
 
+
     # Create and solve problem
     problem = cp.Problem(cp.Minimize(obj), constraints)
-
 
     # try all different parameter combinations
     for _, params in weights_dict.items():
@@ -331,6 +320,16 @@ def optimize_data_center_siting(data, scenario_name, weights_dict, equity_type='
         beta.value = params['beta']
         gamma.value = params['gamma']
         delta.value = params['delta']
+
+        # Validation results
+        if train_path is not None:
+            # read in and set parameter values for x, s, w
+            with open(f"Data/Models/{train_path}/{weights_name}_alpha_{params['alpha']}_beta_{params['beta']}_gamma_{params['gamma']}_delta_{params['delta']}.pkl", "rb") as f:
+                train_results = pickle.load(f)
+
+            train_x.value = train_results['results']['x']
+            train_s.value = train_results['results']['s']
+            train_w.value = train_results['results']['w']
 
         # update params
         params['equity_type'] = equity_type
@@ -510,17 +509,6 @@ def run_optimization(config_file):
     print(f"Running: {config['scenario_name']}")
     print(f"{'='*50}")
 
-    # read data
-    huc8_df = gpd.read_file(config['huc8_df'])
-    solar_proportion_df = pd.read_csv(config['solar_proportion_df'], index_col=0)
-    wind_proportion_df = pd.read_csv(config['wind_proportion_df'], index_col=0)
-    demand_profile = pd.read_csv(config['demand_profile'], index_col=0)
-
-    # prepare data
-    data = prepare_optimization_data(
-        huc8_df, solar_proportion_df, wind_proportion_df, demand_profile, train_path=config['train_path']
-    )
-
     # prepare weights
     weights_df = pd.read_csv(config['weights_file'], index_col=0)
 
@@ -539,7 +527,7 @@ def run_optimization(config_file):
 
     # run the optimization
     optimize_data_center_siting(
-        data, 
+        config,
         scenario_name=config['scenario_name'],
         weights_dict=weights_dict,
         equity_type=config['equity_type'],
